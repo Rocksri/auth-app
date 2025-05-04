ef2644b35d7ac82d16264775b569762d5d56a945
Rocksri <srinathanand1979@gmail.com>
2025-05-04 16:07:05 +0530

    Final

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
+const cors = require('cors');
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Database Connection
+connectDB();
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
